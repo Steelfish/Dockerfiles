a4394175215767dbd9c3cb180d6a04163809da0b
Tom Bakker <t.bakker@odyssion.net>
2021-01-02 16:14:10 +0100

    Add springboot dockerfile

---
+++ springboot-admin/README.md
@@ -0,0 +1,8 @@
+# Springboot-admin
+
+An implementation of springboot admin UI with security features.
+
+Default port: `8001`
+Default user: `admin`
+Default password: `admin`
+URL: `http://localhost:8001`
---
+++ springboot-admin/src/main/java/odyssion/springboot/admin/config/SecurityConfig.java
@@ -0,0 +1,46 @@
+package odyssion.springboot.admin.config;
+
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private static final String REDIRECT_PATH = "redirectTo";
+    private static final String ASSETS_PATH = "/assets/**";
+    private static final String LOGIN_PATH = "/login";
+    private static final String LOGOUT_PATH = "/logout";
+    private final String adminContextPath;
+
+    public SecurityConfig(AdminServerProperties adminServerProperties) {
+        this.adminContextPath = adminServerProperties.getContextPath();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        SavedRequestAwareAuthenticationSuccessHandler successHandler
+                = new SavedRequestAwareAuthenticationSuccessHandler();
+        successHandler.setTargetUrlParameter(REDIRECT_PATH);
+
+        http.authorizeRequests()
+            .antMatchers(adminContextPath + ASSETS_PATH).permitAll()
+            .antMatchers(adminContextPath + LOGIN_PATH).permitAll()
+            .requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll()
+            .anyRequest().authenticated()
+            .and()
+                .formLogin()
+                .loginPage(adminContextPath + LOGIN_PATH)
+                .successHandler(successHandler)
+            .and()
+                .logout()
+                .logoutUrl(adminContextPath + LOGOUT_PATH)
+            .and()
+                .httpBasic()
+            .and()
+                .csrf()
+                .disable();
+    }
+}
